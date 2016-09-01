ce240a129769a5b29e46271c89237a725344bef6
joelbandi <joelvivekbandi@gmail.com>
2016-09-01 09:37:37 -0700

    Reactified the website

---
+++ js/react/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+
+
+class Sample extends React.Component{
+    render(){
+        return (
+            <h2>joelsamplesdcdscsdcds</h2>
+        )
+    }
+}
+
+ReactDOM.render(<Sample/>,document.getElementById('sample'));
