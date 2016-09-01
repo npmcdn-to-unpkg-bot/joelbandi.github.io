11857b2c6c0d149ab24c1226b6add7a5cd4bb17a
joelbandi <joelvivekbandi@gmail.com>
2016-09-01 09:40:00 -0700

    reactified the website

--- js/react/index.jsx
+++ js/react/index.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 class Sample extends React.Component{
     render(){
         return (
-            <h2>joelsamplesdcdscsdcds</h2>
+            <h2>React</h2>
         )
     }
 }
