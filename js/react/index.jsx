95ad5ee6e9994e727cab575791765571db6bd80c
joelbandi <joelvivekbandi@gmail.com>
2016-09-06 02:11:30 -0700

    Added react message at footer

--- js/react/index.jsx
+++ js/react/index.jsx
@@ -5,9 +5,9 @@ import ReactDOM from 'react-dom';
 class Sample extends React.Component{
     render(){
         return (
-            <h2>React</h2>
+            <p>Powered by React js</p>
         )
     }
 }
 
-ReactDOM.render(<Sample/>,document.getElementById('sample'));
+// ReactDOM.render(<Sample/>,document.getElementById('sample'));
